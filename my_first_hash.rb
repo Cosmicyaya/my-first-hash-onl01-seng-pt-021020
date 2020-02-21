ccb6ba4446d1a31b1e6f17d31beca724ad798719
Ryan Hollins <ballbaroi@gmail.com>
2020-02-21 14:47:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,7 +2,8 @@ def my_hash
   
   my_name = {"name" => "Ryan", "age" => "29", "weight" => "129"
 end
-
+def shipping_manifest
+  
 shipping_manifest = {"whale bone corsets" => 5,"porcelain vases" => 2,"oil paintings" => 3}
 end
 
