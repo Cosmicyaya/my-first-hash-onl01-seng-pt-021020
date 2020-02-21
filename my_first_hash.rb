948a0ce29d375f886014f4c0654db9d2b8cba917
Ryan Hollins <ballbaroi@gmail.com>
2020-02-21 14:59:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,5 +16,5 @@ end
 shipping_manifest = {"whale bone corset" => 5,"porcelain vase" => 2,"oil painting" => 3}
 
 shipping_manifest = ["muskets"] = 2
-
-
+shipping_manifest = ["gun powder" = 4]
+end
