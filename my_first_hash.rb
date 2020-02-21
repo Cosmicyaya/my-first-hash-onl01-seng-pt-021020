86f5ff75caccf6cc12a12eba712e128392de966c
Ryan Hollins <ballbaroi@gmail.com>
2020-02-21 15:08:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,10 +7,7 @@ def shipping_manifest
   
 shipping_manifest = {"whale bone corsets" => 5,"porcelain vases" => 2,"oil paintings" => 3}
 end
-shipping_manifest = {
-  "whale bone corset" => 5,
-  "porcelain vase" => 2,
-  "oil painting" => 3}
+shipping_manifest = {"whale bone corset" => 5,"porcelain vase" => 2, "oil painting" => 3}
   
 shipping_manifest ["oil paintings"]
  end
