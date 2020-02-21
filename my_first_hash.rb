c6a920f6dc4ad63b55dba5c81f62cf6fe9956b5e
Ryan Hollins <ballbaroi@gmail.com>
2020-02-21 14:46:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   
-  my_name = {"name" => "R"
+  my_name = {"name" => "Ryan", "age" => "29", "weight" => "129"
 end
 shipping_manifest = {"whale bone corsets" => 5,"porcelain vases" => 2,"oil paintings" => 3}
 end
